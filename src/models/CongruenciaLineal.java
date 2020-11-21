ad871e525ef31c4f49df151f31ef827139123c9a
Oscar Rojas <rojascruzoscar@gmail.com>
2020-11-21 09:11:49 -0500

    Metodo congruencia lineal genera Ri y genera Xi

---
+++ src/models/CongruenciaLineal.java
@@ -0,0 +1,66 @@
+package models;
+
+public class CongruenciaLineal extends PseudoRandom {
+
+	/* Semilla x0 */
+	private int Xi;
+
+	/* Interseccion de la recta */
+	private int c;
+
+	/* a */
+	private int a;
+
+	/* m */
+	private int m;
+
+	/**
+	 * Actualiza los valores usados para congruencia lineal
+	 * 
+	 * @param k pendiente de la recta
+	 * @param c interseccion de la recta
+	 * @param g periodo de generación (se convertira en m)
+	 */
+	public void updateCongruencia(int k, int c, int g) {
+		Xi = super.seed;
+		this.c = c;
+
+		a = 1 + (2 * k);
+		m = (int) Math.pow(2, g);
+	}
+
+	public void algorithm() {
+		for (int i = 0; i < super.numbersToGenerate; i++) {
+			Xi = ((a * Xi) + c) % m;
+			double Ri = generateRi(Xi, true);
+
+			System.out.println("Xi : " + Xi + "-- Ri : " + Ri);
+		}
+	}
+
+	/**
+	 * Genera los numeros Ri
+	 * 
+	 * @param Xi          el numero pseudo aleatorio generado inicialmente para
+	 *                    generar Ri
+	 * @param isClosedSet si es conjunto cerrado aplica la formula (xi / m-1) si no
+	 *                    aplica (xi / m)
+	 * @return el numero Ri generado
+	 */
+	public double generateRi(int Xi, boolean isClosedSet) {
+		double auxXi = (double) Xi;
+		if (isClosedSet) {
+			return (auxXi / (m - 1));
+		}
+		return (auxXi / m);
+	}
+
+	public static void main(String[] args) {
+		CongruenciaLineal congruenciaLineal = new CongruenciaLineal();
+
+		congruenciaLineal.updateValues(8, 10, 1, 10);
+		congruenciaLineal.updateCongruencia(4, 3, 7);
+
+		congruenciaLineal.algorithm();
+	}
+}
--- src/models/PseudoRandom.java
+++ src/models/PseudoRandom.java
@@ -4,8 +4,16 @@ public abstract class PseudoRandom {
 
 	public int minLimit;
 	public int maxLimit;
-	public int Xn;
-	
+	public int seed;
+	public int numbersToGenerate;
+
+	public void updateValues(int minLimit, int maxLimit, int seed, int numbersToGenerate) {
+		this.minLimit = minLimit;
+		this.maxLimit = maxLimit;
+		this.seed = seed;
+		this.numbersToGenerate = numbersToGenerate;
+	}
+
 	public double calculateNI(int Ri) {
 		return minLimit + (maxLimit - minLimit) * Ri;
 	}
