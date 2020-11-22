aaa99c6cb028b3653d3f284ce0afd5838f6947f3
Oscar Rojas <rojascruzoscar@gmail.com>
2020-11-21 20:04:43 -0500

    Adding constants file for view properties

---
+++ src/constants/MyConstants.java
@@ -0,0 +1,26 @@
+package constants;
+
+public class MyConstants {
+
+	public static final String APP_TITLE = "Generador de números pseudoaleatorios";
+
+	// Labels
+	public static final String LBL_TITLE = "<html>Generador de números<br>pseudoaleatorios</html>";
+
+	// Buttons
+	public static final String BTN_CUADRADOS_MED = "Cuadrados Medios";
+	public static final String BTN_CONGRUENCIA_L = "Congruencia Lineal";
+	public static final String BTN_CONGRUENCIA_M = "Congruencia Multiplicativa";
+	public static final String BTN_DISTRO_UNIFOR = "Distribución Uniforme";
+	public static final String BTN_DISTRO_NORMAL = "Distribución Normal Estándar";
+	public static final String BTN_ACERCA_DE = "Acerca de";
+
+	// Fonts
+	public static final String FONT_ROBOTO = "Roboto";
+
+	// Hexadecimal
+	public static final String CLR_BLUE_BTNS = "#07689F";
+	public static final String CLR_YELLOW_PANEL = "#07689F";
+	public static final String CLR_BLACK = "#000000";
+	public static final String CLR_WHITE = "#FFFFFF";
+}
